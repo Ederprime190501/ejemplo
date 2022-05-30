4e06a5dbb202f568decf221989e2de0204d5b6b9
Ederprime190501 <88561414+Ederprime190501@users.noreply.github.com>
2022-05-29 20:45:55 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Arte Urbano | 2022</title>
+    <link rel="stylesheet" href="recursos/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="recursos/fontawesome/css/all.css">
+    <link rel="stylesheet" href="recursos/css/styles.css">
+    <link rel="stylesheet" href="recursos/css/animate/animate.css">
+</head>
+<body>
+<!--Menu de Navegación-->
+  <div>
+   <?php include'recursos/includes/nav.php' ?>
+    </div>
+    
+<!--Carrusel de Imagenes-->
+    <div class="banner">
+		<div class="capa"></div>
+		<div class="info">
+			<h1>Bienvenidos a FanArt.com</h1>
+			<p>Visita nuestro sitio web sobre la promocionar el Arte Urbano y animate a adquirir o ser proveedor de productos o servicios.</p>
+			<a href="#">Leer Màs</a>
+		</div>
+	</div>
+	
+<!--Antecedcentes-->
+   <div class="antecedentes">
+       <h2 class="h3">Antecedentes</h2>
+       <p class="lh-base text-justify">El graffiti invasivo también conocido como Arte Urbano es un fenómeno que vulnera el patrimonio cultural y el paisaje urbano del Centro Histórico de la Ciudad de México, al realizarse de manera masiva e incontrolada en distintos espacios de esta zona de la ciudad declarada Patrimonio Cultural de la Humanidad por la Unesco. Sin embargo, el graffiti puede alcanzar altas cotas de calidad, convirtiéndose en una forma de expresión artística con una sensibilidad única para transmitir el pulso de la urbe. De esta manera, resignifica espacios inexpresivos a través del deleite estético, y siempre pretende sorprender de manera positiva a sus espectadores. 
+       Con el fin de combatir el grafiti invasivo en la zona patrimonial del Centro Histórico de la Ciudad de México se desarrolló esta aplicación para que este fenómeno conocido como Arte Urbano siga siendo de nivel masivo, pero también controlado es decir legalmente en toda la republica Mexicana.
+       </p>
+       <a class="btn btn-outline-success boton" href="conocenos.php">Conocenos</a>
+   </div>
+   <hr>
+<!--Publicaciones-->
+    <div class="publicaciones">
+     <h2 class="h3 d-inline">Comunidad</h2>
+     <p class="d-inline leyenda">Publicaciones más recientes</p><br><br>
+      <div class="row">
+        <div class="col-12 col-md-6 col-lg-4">
+          <div class="card">
+            <img src="recursos/img/imagenes/arte6.jpg" alt="imagen1" class="card-img-top public_img">
+            <div class="card-body">
+              <h5 class="card-title">Murales decorativos para exterior</h5>
+              <p class="card-text texto_public">Los graffitis murales decorativos para exterior son las obras que se realizan al aire libre. Abraza desde el gran formato a pequeñas superficies que normalmente serán en vertical, es decir, trabajar sobre una pared adecuándose al paisaje urbano. <br> Ten en cuenta que, un mural decorativo exterior, cuanto más grande sea, los costes de ejecución aumentarán por la necesidad de utilizar herramientas incomodas para el trabajo o la maquinaria elevadora.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+          <div class="card">
+            <img src="recursos/img/imagenes/arte7.jpg" alt="imagen1" class="card-img-top public_img">
+            <div class="card-body">
+              <h5 class="card-title">Murales decorativos para interior</h5>
+              <p class="card-text texto_public"> Los clientes pueden elegir entre un diseño afuera o hacerlo en un espacio interior; entre los cuatro muros del hogar, en un paramento de un restaurante, parkings, local de fiestas, pubs o incluso en pasillos, entradas (halls) y grandes salones de edificios.
+              Por supuesto, el ocio nocturno es el principal exponente de la decoración artística con graffiti para interior, pero, desde una perspectiva del propio interiorismo, es un estilo rompedor cuando lo trasladamos a la vivienda y se plasma en algún proyecto.¡grafiteros a domicilio! Es la decoración con graffiti para el interior de una casa. </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+          <div class="card">
+            <img src="recursos/img/imagenes/arte8.jpg" alt="imagen1" class="card-img-top public_img">
+            <div class="card-body">
+              <h5 class="card-title">Murales Geometricos</h5>
+              <p class="card-text texto_public">Si eres un amante de las matemáticas, los artistas también tienen la habilidad de poder “jugar” con un muro y aplicar las mismas reglas espaciales que se usarían en el plano.Utilizan figuras poligonales de distinto tipo; incluyendo triángulos, cuadrados, rombos…¡todo tipo de geometrías! Si bien es cierto que muchos profesionales tienen la capacidad de elaborar líneas rectas a mano alzada, sin ningún tipo de ayuda extra, otros tantos artistas utilizan cinta de carrocero para hacer formas perfectas.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+  <hr>
+  
+  <!--Productos-->
+  <div class="productos">
+     <h2 class="h3 d-inline">Productos</h2>
+     <p class="d-inline leyenda">¡Adquiere YA nuesros productos de calidad!</p><br><br>
+      <div class="row">
+        <div class="col-12 col-md-6 col-lg-4">
+         <div class="card">
+            <img src="recursos/img/productos/PINTURAS/pinturas.jpg" alt="imagen1" class="card-img-top public_img">
+            <div class="card-body">
+              <h5 class="card-title">Pinturas</h5>
+    <p class="card-text text_product">Adquiere pinturas  en aerosol de alta calidad para la realización de tu trabajo artisitco urbano.</p>
+    <a href="prodPinturas.php" class="btn btn-outline-success">Ver mas</a>
+            </div>
+          </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-4">
+         <div class="card">
+            <img src="recursos/img/productos/ACCESORIOS/accesorios.png" alt="imagen1" class="card-img-top public_img">
+            <div class="card-body">
+              <h5 class="card-title">Accesosrios Extras</h5>
+    <p class="card-text">Adquiere cualquier tipo de accesorios de alta calidad que te serviran como herramientas de trabajo.</p>
+    <a href="prodAccesorios.php" class="btn btn-outline-success">Ver mas</a>
+            </div>
+          </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-4">
+         <div class="card">
+            <img src="recursos/img/productos/SOLVENTES/solventes.png" alt="imagen1" class="card-img-top public_img">
+            <div class="card-body">
+              <h5 class="card-title">Solventes</h5>
+    <p class="card-text">Adquiere cualquier tipo de solvente de alta calidad para la realización de tu trabajo artisitco urbano.</p>
+    <a href="prodSolventes.php" class="btn btn-outline-success">Ver mas</a>
+            </div>
+          </div>
+          </div>
+      </div>
+    </div>
+    
+<!--Footer-->
+   <div>
+   <?php include'recursos/includes/footer.php' ?>
+    </div>
+    
+    
+    <script src="recursos/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="recursos/fontawesome/js/all.js"></script>
+    <script src="recursos/js/jquery.min.js"></script>
+</body>
+</html>
